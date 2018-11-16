d3e26ac4e2bdec1591b9615f71a842c86871fd19
JuliusVirtanen <32675222+JuliusVirtanen@users.noreply.github.com>
2018-11-16 18:07:00 +0200

    Sample-geojson tiedoston muokkaus
    Patsaat siirretty omaan tiedostoon

--- sample-geojson.js
+++ sample-geojson.js
@@ -79,7 +79,7 @@ var lightRailStop = {
 var kultuuriKohteet = {
     "type": "FeatureCollection",
     "features": [
-	
+
         {
             "geometry": {
                 "type": "Point",
@@ -93,105 +93,6 @@ var kultuuriKohteet = {
                 "popupContent": "Kakolan vanha vankila"
             },
             "id": 52
-        },
-        {
-            "geometry": {
-                "type": "Point",
-                "coordinates": [
-                    22.2698455,
-                    60.4553247
-                ]
-            },
-            "type": "Feature",
-            "properties": {
-                "popupContent": "Kun ystävyyssuhteet solmitaan patsas"
-            }
-        },
-        {
-            "geometry": {
-                "type": "Point",
-                "coordinates": [
-                    22.2625100,
-                    60.453764
-
-                ]
-            },
-            "type": "Feature",
-            "properties": {
-                "popupContent": "Victor Westerholmin muotokuvahermi"
-            }
-        },
-        {
-            "geometry": {
-                "type": "Point",
-                "coordinates": [
-                    22.2620063,
-          60.4536163
-
-                ]
-            },
-            "type": "Feature",
-            "properties": {
-                "popupContent": "Robert W. Ekmanin muotokuvahermi "
-            }
-        },
-        {
-            "geometry": {
-                "type": "Point",
-                "coordinates": [
-                    22.2716215,
-                    60.4486574
-
-                ]
-            },
-            "type": "Feature",
-            "properties": {
-                "popupContent": "Turun Lilja -patsas"
-            }
-        },
-            {
-            "geometry": {
-                "type": "Point",
-                "coordinates": [
-                    22.2701924,
-          60.4480107
-
-        ]
-            },
-            "type": "Feature",
-            "properties": {
-                "popupContent": "Paavo Nurmen patsas"
-            }
-        },
-        {
-            "geometry": {
-                "type": "Point",
-                "coordinates": [
-                    22.2647324,
-                    60.4465085
-
-                ]
-            },
-            "type": "Feature",
-            "properties": {
-                "popupContent": "Aleksis Kiven muistomerkki"
-            }
-        },
-        {
-            "geometry": {
-                "type": "Point",
-                "coordinates": [
-                    22.2254193,
-                    60.4274832
-
-                ]
-            },
-            "type": "Feature",
-            "properties": {
-                "popupContent": "Muistijälki teos"
-            }
         }
     ]
 };
-
-	
