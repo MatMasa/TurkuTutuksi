7c203ce449445d59da28a872274013119bde148f
Junjan <jan.stockfelt.@edu.turkuamk.fi>
2018-11-18 13:06:15 +0200

    Versio 1.0 commit

--- Scripts/checkboxFunctions.js
+++ Scripts/checkboxFunctions.js
@@ -1,6 +1,7 @@
 function kirkkoFunction() {
 			var checkbox = document.getElementById("myCheck");
 				if(checkbox.checked == true) {
+					mymap.addLayer(kirkot)
 					mymap.removeLayer(pyörät);
 					mymap.removeLayer(museot);
 					mymap.removeLayer(veistokset);
@@ -16,6 +17,7 @@ function kirkkoFunction() {
 		  function museoFunction() {
 			var checkbox = document.getElementById("myCheck1");
 				if(checkbox.checked == true) {
+					mymap.addLayer(museot)
 					mymap.removeLayer(pyörät);
 					mymap.removeLayer(kirkot);
 					mymap.removeLayer(veistokset);
@@ -30,6 +32,7 @@ function kirkkoFunction() {
 		  function pyörätFunction() {
 			var checkbox = document.getElementById("myCheck2");
 				if(checkbox.checked == true) {
+					mymap.addLayer(pyörät)
 					mymap.removeLayer(museot);
 					mymap.removeLayer(kirkot);
 					mymap.removeLayer(veistokset);
