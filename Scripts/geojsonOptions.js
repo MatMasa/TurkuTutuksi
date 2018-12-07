e77259ef835e2762973d3d6e1b23dfa659ee587f
Julius Virtanen <julius.virtanen@edu.turkuamk.fi>
2018-12-07 17:32:28 +0200

    Popup sisällön muokkaus
    Fölläri-pysäkkien kuva ja linkki lisätty

--- Scripts/geojsonOptions.js
+++ Scripts/geojsonOptions.js
@@ -14,7 +14,9 @@ geojsonOpts = {
 
 		geojsonOpts_3 = {
 			pointToLayer: function(feature, latlng) {
-			return L.marker(latlng,{icon:bikeIcon}).bindPopup(feature.properties.popupContent)}
+			return L.marker(latlng,{icon:bikeIcon}).bindPopup(feature.properties.popupContent +
+              	"</strong><br/>" + "<img src=" + "https://www.aamuset.fi/static/content/pic_6_3811339_k2728021_1200.jpg" + "" + " class=popupImage " + "/>" +
+								"</strong><br/>" + "<a href='" + "https://www.foli.fi/en/citybikes"  + "'target=\'_blank\'>" + "Tietoja kohteesta" + "</a>")}
 		}
 
 		geojsonOpts_4 = {
